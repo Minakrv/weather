501c5519d48893dbb1dcb884dedbc663debec7b2
Minakrv <mnkrv@yahoo.com>
2021-06-17 12:06:56 +0100

    creating server setting

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const fetch = require('node-fetch');
-require('dotenv').config();
+const express = require('express');
+const hbs = require('express-handlebars');
+const path = require('path');
+const getWeather = require('./lib/getweather');
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('hbs', hbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials')
+}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engin', '.hbs');
+app.get('/', async(req, res) => {
+    let data = await getWeather();
+    let name = data.name;
+    let temp = data.main.temp;
+    res.render('index', {name, temp});
+});
+app.get('/weather', (req, res)=>{
+    res.render('weather');
+});
+app.get('*', (req, res)=>{
+    res.render('404');
+});
 
-const url = `http://api.openweathermap.org/data/2.5/weather?q=Manchester&appid=${process.env.APPID}`;
-
-const getWeather = async()=> {
-    let data = await fetch(url);
-    console.log(await data.json());
-};
-
-getWeather();
+app.listen(3000 , ()=>{
+    console.log('listening on port 3000');
+});
