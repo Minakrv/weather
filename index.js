c3a4d364ea239969b56c993a7a75ba1858bafc6a
Minakrv <mnkrv@yahoo.com>
2021-06-17 11:11:19 +0100

    changes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fetch = require('node-fetch');
-const url = `http://api.openweathermap.org/data/2.5/weather?q=Manchester&appid=2df125ed5ea250c03e42147f1dd09c77&units=metric`;
+require('dotenv').config();
+
+const url = `http://api.openweathermap.org/data/2.5/weather?q=Manchester&appid=${process.env.APPID}`;
 
 const getWeather = async()=> {
     let data = await fetch(url);
